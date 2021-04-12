4c2e3ba141c50f84a5ecd807f8d51688225945c6
nik.volgemut <nik.volgemut@student.um.si>
2021-04-12 19:53:26 +0200

    feat(contentScript): wip message handling + basic injection logic

--- src/plugin.js
+++ src/plugin.js
@@ -1,53 +1,77 @@
+const extensionId = "oiv@addon.com";
+
 /*
-    outlook divs
+    Outlook <div> class names.
  */
 const newMsg = "ms-Button _33rLSYbzxvhXjgYTwfjWQI _1yXkaiMFfrJerhRD_mw8tk _1ojerECVeZlAkHtklkKOj5 ms-Button--commandBar root-54";
 const buttonRow = "ivs3kF0TSy1MNYEjC_hAw";
 const contentDiv = "_4utP_vaqQ3UQZH0GEBVQe B1QSRkzQCtvCtutReyNZ CAUXSSmBTHvYTez0U6p3M _17ghdPL1NLKYjRvmoJgpoK _2s9KmFMlfdGElivl0o-GZb";
 const recipientSpan = "ReadWriteCommonWell-wellItemText wellItemText-228";
 
-console.log("Loaded script");
+console.log("Loaded content script");
 
 /*
-    config
-    TODO: replaced by actual implementation(Menu)
+    Injection logic.
  */
-const enabled = true;
-// let key = [
-//     new PrivateKey("","","","",""),
-//     new PublicKey("","","","")
-// ];
-//
-// let publicKeys = [
-//     new PublicKey("","","","")
-// ];
+
+/**
+ * Creates a promise that waits for an element to load into the DOM.
+ *
+ * @param selector The selector of the element we are waiting for.
+ * @returns {Promise<unknown>} The promise.
+ */
+const waitForElm = (selector) => {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 
 window.addEventListener("load", evt => {
     console.log("Added onLoad");
-    if (enabled) {
+    //TODO: implement checking the settings.enabled boolean
+    if (true) {
         console.log("entered enabled block");
-        const newMsgButton = document.getElementsByClassName(newMsg).item(0);
-        console.log(newMsgButton);
-        if (newMsgButton) {
-            newMsgButton.addEventListener("click", evt => {
+
+        waitForElm("." + newMsg).then(elm => {
+            console.log(elm.textContent);
+            elm.addEventListener("click", evt => {
                 console.log("Triggered onClick event");
-                // try{
-                //     const buttonRowElement = document.getElementsByClassName(buttonRow).item(0);
-                //     buttonRowElement.addEventListener("load", evt1 => {
-                //         console.log("foo");
-                //         buttonRowElement.innerHTML += "TEST";
-                //     });
-                // }catch (e){
-                //     console.error(e.message);
-                // }
+                waitForElm("." + buttonRow).then(elm1 => {
+                    console.log(elm1.textContent);
+                    //FIXME: accurate button html
+                    elm1.innerHTML += "TEST";
+                })
             });
-            console.log("Added listener.")
-        }
+        });
+
     }
 });
 
+/*
+    Main extension logic.
+ */
+/**
+ * Extracts senders email address from the appropriate div element.
+ *
+ * @returns {string} The extracted email.
+ */
 const getRecipientEmail = () => {
-    //Simon Sambolec <simon.sambolec@student.um.si>
+    //Ime Priimek <ime.priimek@student.um.si>
     const recipientSpanElement = document.getElementsByClassName(recipientSpan).item(0);
     if (recipientSpanElement) {
         const text = recipientSpanElement.innerHTML;
@@ -56,9 +80,63 @@ const getRecipientEmail = () => {
 
         return foo.substring(1, foo.length - 1);
     }
+
+    return "";
 }
 
-const sendMessage = (obj) => {
-    obj = obj || {};
-    browser.runtime.sendMessage(obj);
+browser.runtime.onMessage.addListener(handleMessage);
+
+/**
+ * onMessage event handler
+ *
+ * @param data {any?} Message content
+ * @param sender {object} Sender info
+ * @param sendResponse {function} A callback which enables you to send data back.
+ */
+const handleMessage = (data, sender, sendResponse) => {
+    data = data || {};
+    console.debug(`Incoming from ${sender} with content: ${data}`);
+
+    switch (data.cmd) {
+        case "ping":
+            sendResponse("pong");
+            break;
+        default:
+            console.debug("Invalid command code");
+    }
+};
+
+/**
+ * Send a message object to all onMessage listeners with a specified message type.
+ *
+ * @param cmd {String} Determines how the message will be handled by the receiver.
+ * @param msg {any} The message.
+ * @param responseHandler {function} A function that will handle the response.
+ */
+const sendMessage = (cmd, msg, responseHandler) => {
+    let obj = {
+        cmd: cmd || "unknown",
+        msg: msg || ""
+    }
+
+    browser.runtime
+        .sendMessage(extensionId, obj)
+        .then((response) => {
+            if(responseHandler){
+                responseHandler(response);
+            }else{
+                defaultResponseHandler(response);
+            }
+        }).catch((e) => {
+            log.error(e);
+    });
 }
+
+/**
+ * Handles the incoming message when a custom responseHandler is not defined.
+ *
+ * @param message The message.
+ */
+const defaultResponseHandler = (message) => {
+    console.debug(`Response recieved: ${message}`);
+};
--- view/background.js
+++ view/background.js
@@ -1,1 +1,23 @@
-//TODO: "backend" logic
+import {PublicKey, PrivateKey} from "../src/containers/key";
+import * as openpgp from "../lib/openpgp.min.mjs";
+
+//TODO: settings persistence
+const settings = {
+    enabled: true,
+    publicKeys: [
+        new PublicKey("", "", "", ""),
+        new PublicKey("", "", "", "")
+    ],
+    userKeys: {
+        "public": new PublicKey("", "", "", ""),
+        "private": new PrivateKey("", "", "", "", "")
+    }
+};
+
+const sendMessage = (obj) => {
+
+};
+
+const encrypt = async () => {
+
+};
